f668c371eb38d3fb4f8517022bdbc58ba72a837e
Rich Capuy <rccapuy@gmail.com>
2020-07-26 17:55:03 -0400

    Map employees and display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import Header from './components/Header/';
 import Wrapper from './components/Wrapper/';
+import EmployeeCard from './components/EmployeeCard/'
 import Employees from './employees.json';
 
 function App() {
-  const [employees, setEmplyees] = useState({Employees});
+  const [employees, setEmployees] = useState({
+    Employees
+  });
+
   return (
     <div className="App">
       <Header />
       <Wrapper>
-        
+      {Employees.map(employee => (
+          <EmployeeCard
+            key={employee.id}
+            id={employee.id}
+            name={employee.name}
+            image={employee.image}
+            occupation={employee.occupation}
+          />
+        ))}
       </Wrapper>
     </div>
   );
