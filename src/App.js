c6a49e2c6f81bdcc229cc41f843fabbf52881cc4
Rich Capuy <rccapuy@gmail.com>
2020-07-26 16:33:34 -0400

    importing wrapper component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header/';
+import Wrapper from './components/Wrapper/'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Wrapper>
+        
+      </Wrapper>
     </div>
   );
 }
