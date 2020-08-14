65d931d649b8bdd1264f8f796c6623c2e191b16c
Rich Capuy <rccapuy@gmail.com>
2020-08-14 09:27:20 -0400

    Updates

---
+++ precache-manifest.b6357efb2d381458d934ee406f8b26c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e86952573450c7362be02c300f0544",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "c7be5c0f23f1afeaa415",
+    "url": "/Employee-Directory/static/css/main.92c8422d.chunk.css"
+  },
+  {
+    "revision": "cd1e4e401fc4b8f9a7fb",
+    "url": "/Employee-Directory/static/js/2.f52948d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.f52948d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7be5c0f23f1afeaa415",
+    "url": "/Employee-Directory/static/js/main.8648f7cf.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
